def81936b6fc798764480cdf23b357df54a77569
Rui Xiao <ruixiao85@gmail.com>
2024-06-22 15:02:59 -0700

    1.3.0 opal side panel increase contrast.

--- installation.bash
+++ installation.bash
@@ -1,6 +1,9 @@
+conda create -c conda-forge -n node nodejs
+conda activate node
 
 npm install -g vsce
 
+# make edits to themes and update package version
 vsce package
 
 # https://ruixiao85.visualstudio.com/ look for settings/personal token
